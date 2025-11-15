e9df97a45a5e29b67d3a7ee075237c063d0709ea
BalinValery <valera.balin00@mail.ru>
2025-11-15 05:05:28 +0500

    Task2

---
+++ Tyuiu.BalinVV.Sprint3.Task2.V20.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.BalinVV.Sprint3.Task2.V20.Lib;
+namespace Tyuiu.BalinVV.Sprint3.Task2.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 6;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 12353130442653.422;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
