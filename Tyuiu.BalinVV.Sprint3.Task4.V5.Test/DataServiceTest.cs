9de3c079e4a259c45434233185fe363bd817239e
BalinValery <valera.balin00@mail.ru>
2025-11-15 10:39:27 +0500

    Task4

---
+++ Tyuiu.BalinVV.Sprint3.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BalinVV.Sprint3.Task4.V5.Lib;
+namespace Tyuiu.BalinVV.Sprint3.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int start = -5, end = 5;
+            double res = ds.Calculate(start, end);
+            Assert.AreEqual(-0.858, res);
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint3.Task4.V5/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.BalinVV.Sprint3.Task4.V5.Lib;
+internal class Program
+{
+     static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int startValue = -5, endValue = 5;
+        Console.Title = "Спринт #3 | Выполнил: Балин В.В. | СМАРТб-25-1";
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("*Спринт 3                                                                  *");
+        Console.WriteLine("*Тема: Оператор цикла foreach                                              *");
+        Console.WriteLine("*Задание #4                                                                *");
+        Console.WriteLine("*Вариант #5                                                                *");
+        Console.WriteLine("*Выполнил: Балин В.В.| СМАРТб-25-1                                         *");
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5,                         *");
+        Console.WriteLine("* вычислить значение функции y=(cos(x)-sin(x))/x                           *");
+        Console.WriteLine("* При х = 0 прервать цикл. Полученные значения суммировать.                *");
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* -5 <= x <= 5                                                             *");
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* Результат:                                                               *");
+        Console.WriteLine("****************************************************************************");
+
+        Console.Write("Сумма функций равна " + ds.Calculate(startValue, endValue));
+        Console.ReadLine();
+    }
+}
