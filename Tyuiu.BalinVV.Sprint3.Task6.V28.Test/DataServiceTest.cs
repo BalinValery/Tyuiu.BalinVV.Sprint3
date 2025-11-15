c40384a3bbb70a6690d2276c8c8a1e99cd3cc2f9
BalinValery <valera.balin00@mail.ru>
2025-11-15 11:09:08 +0500

    Task6

---
+++ Tyuiu.BalinVV.Sprint3.Task6.V28.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.BalinVV.Sprint3.Task6.V28.Lib;
+namespace Tyuiu.BalinVV.Sprint3.Task6.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 13;
+            int stopValue = 19;
+
+            int result = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 15; 
+
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint3.Task6.V28/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.BalinVV.Sprint3.Task6.V28.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Балин В.В. | СМАРТб-25-1";
+       
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #28                                                             *");
+        Console.WriteLine("* Выполнил: Балин В.В.| СМАРТб-25-1                                       *");
+        Console.WriteLine("введите начальное значение отрезка: ");
+        int startValue = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("введите конечное значение отрезка: ");
+        int stopValue = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        
+
+        var result = ds.GetSumTheDivisors(startValue, stopValue);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
