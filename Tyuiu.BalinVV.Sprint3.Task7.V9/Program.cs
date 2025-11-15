9533a470c804da9c755b96da0a739966298836ae
BalinValery <valera.balin00@mail.ru>
2025-11-15 11:21:45 +0500

    Task7

---
+++ Tyuiu.BalinVV.Sprint3.Task7.V9/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.BalinVV.Sprint3.Task7.V9.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Балин В.В. | СМАРТб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема:  Добавление итоговых проектов по спринту                          *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #9                                                              *");
+        Console.WriteLine("* Выполнил: Балин В.В.| СМАРТб-25-1                                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = -5;
+        int stopValue = 5;
+
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+        double[] valueArray;
+        valueArray = new double[len];
+
+        valueArray = ds.GetMassFunction(startValue, stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("РЕЗУЛЬТАТ:                                                                 ");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("+----------+----------+");
+        Console.WriteLine("|     X    +   f(x)   +");
+        Console.WriteLine("+----------+----------+");
+
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine("|{0,5:d}     |  {1,5:f2}  |", startValue, valueArray[i]);
+            startValue++;
+        }
+        Console.WriteLine("+----------+----------+");
+        Console.ReadKey();
+    }
+}
