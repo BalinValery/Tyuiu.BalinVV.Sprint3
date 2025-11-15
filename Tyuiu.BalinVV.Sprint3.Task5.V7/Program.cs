dfea8c3a7bdee0b0303066c43732dad022e51359
BalinValery <valera.balin00@mail.ru>
2025-11-15 10:49:38 +0500

    Task5

---
+++ Tyuiu.BalinVV.Sprint3.Task5.V7/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.BalinVV.Sprint3.Task5.V7.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Балин В.В. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*Спринт 3                                                                 *");
+        Console.WriteLine("*Тема: Вложенные циклы                                                    *");
+        Console.WriteLine("*Задание #5                                                               *");
+        Console.WriteLine("*Вариант #7                                                               *");
+        Console.WriteLine("*Выполнил: Балин В.В.| СМАРТб-25-1                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Исходные данные");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 5;
+        int startValue1 = 1;
+        int stopValue1 = 3;
+        int startValue2 = 1;
+        int stopValue2 = 10;
+
+        Console.WriteLine("Переменная Х = " + x);
+        Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+        Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+        Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+        Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("РЕЗУЛЬТАТ");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Сумма сумм ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+        Console.ReadKey();
+    }
+}
